ddf791fa6c687c8f0a6e83d34bd3c1fc66da10b3
Павел <pavlinickus@gmail.com>
2023-03-26 17:03:07 +0200

    Override CalculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -47,5 +47,14 @@ namespace GradeBook.GradeBooks
             }
             base.CalculateStatistics();
         }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
