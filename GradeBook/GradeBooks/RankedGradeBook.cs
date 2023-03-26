b9764bf652fd5980b26d787db48183f212c92cfb
Павел <pavlinickus@gmail.com>
2023-03-26 16:56:05 +0200

    Override RankedGradeBook - GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,32 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numberOfStudents = Students.Count;
+            if (numberOfStudents < 5)
+                throw new InvalidOperationException();
+
+            int betterStudents = 0;
+            foreach (Student student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                    betterStudents++;
+            }
+
+            double percentage = (100 * betterStudents) / numberOfStudents;
+
+
+            if (percentage < 20)
+                return 'A';
+            else if (percentage < 40)
+                return 'B';
+            else if (percentage < 60)
+                return 'C';
+            else if (percentage < 80)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
